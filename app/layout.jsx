d6b2e1820b9142b237026a9b011a5c28147538c8
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-01-26 19:04:18 -0500

    Added hero, infoboxes, and footer components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 // Sets metadata for the entire application
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
